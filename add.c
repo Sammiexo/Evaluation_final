498092334b136788a6c0622fd6c106f23dc161f2
Sam <148611213+Sammiexo@users.noreply.github.com>
2024-02-20 15:26:03 +0530

    Update add.c

--- add.c
+++ add.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    int num1, num2, sum;
     
-    printf("Enter the very first number: ");
-    scanf("%d", &num1);
     
     printf("Enter the second number: ");
     scanf("%d", &num2);
